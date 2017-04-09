48b9bbb83e5ba1fdf6b3b13286571f405ff047de
BuildTools <davidfuller123@icloud.com>
2017-04-08 20:00:12 -0400

    randy change
    dw about it
    
    Planning on making methods for Stock, Knockback and GameActive

--- src/us/davidiv/Smash/SSMMelee/Game/CooldownManager.java
+++ src/us/davidiv/Smash/SSMMelee/Game/CooldownManager.java
@@ -1,11 +1,7 @@
 package us.davidiv.Smash.SSMMelee.Game;
 
-/**
- * Created by David on 3/19/2017.
- */
 public class CooldownManager {
 
-    //TODO ADD COOLDOWN METHOD
     //TODO ADD COOLDOWN DISPLAY METHOD
 
 }
