3d038163e3f05df499fe80739b9dd45348491e7b
Davidiv <davidfuller123@icloud.com>
2017-04-11 01:22:27 -0400

    SlimeToss UpdateEvent
    Slime Toss has been completely redone. It now works using UpdateEvent so
    we don't have to deal with BukkitRunnable bugs.
    
    Much easier to read and edit. The original code has been put in OGST in
    case I need to compare values.

--- src/us/davidiv/Smash/SSMMelee/Kit/Slime/SlimeToss.java
+++ src/us/davidiv/Smash/SSMMelee/Kit/Slime/SlimeToss.java
@@ -1,11 +1,16 @@
 package us.davidiv.Smash.SSMMelee.Kit.Slime;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Slime;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 import us.davidiv.Smash.SSMMelee.Events.UpdateEvent;
 import us.davidiv.Smash.SSMMelee.Events.UpdateType;
@@ -13,14 +18,10 @@ import us.davidiv.Smash.SSMMelee.Game.Knockback;
 import us.davidiv.Smash.SSMMelee.Kit.Kits;
 import us.davidiv.Smash.SSMMelee.SmashMelee;
 
+import java.util.HashMap;
+
 import static us.davidiv.Smash.SSMMelee.Game.GameScoreboard.updateSmashScoreboard;
 import static us.davidiv.Smash.SSMMelee.Game.Kit.getKit;
-
-/*
-Slime Rocket rip-off xD!
- */
-
-//recreate with updateevent
 
 public class SlimeToss implements Listener {
     public SlimeToss(SmashMelee plugin) {
@@ -29,7 +30,81 @@ public class SlimeToss implements Listener {
 
     private double exp = 0.999;
     private int l = 0;
-    //Change to hashmap???
+
+    private HashMap<Player, Boolean>rc = new HashMap<>();
+    private HashMap<Player, Integer>charge = new HashMap<>();
+    private HashMap<Player, Integer>setcharge = new HashMap<>();
+
+    @EventHandler
+    public void rightclick(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        ItemStack i = e.getPlayer().getItemInHand();
+
+        if (getKit(p) != Kits.SLIME) {return;}
+
+        if (i.getType() != Material.IRON_SWORD) {return;}
+
+        if(e.getAction() != Action.RIGHT_CLICK_AIR && e.getAction() != Action.RIGHT_CLICK_BLOCK){return;}
+
+        rc.put(p, true);
+    }
+
+    @EventHandler
+    public void charge(UpdateEvent e) {
+
+        if (e.getType() != UpdateType.TICK) {return;}
+
+        for (Player p : Bukkit.getOnlinePlayers()) {
+
+            if (!rc.get(p)) {return;}
+
+            if (getKit(p) != Kits.SLIME) {return;}
+
+            if (!p.isBlocking()) {
+                setcharge.put(p, getCharge(p));
+                charge.remove(p);
+                rc.put(p, false);
+                return;
+            }
+
+            p.setExp((p.getExp() - .02f));
+            addCharge(p, 1);
+            p.getWorld().playSound(p.getLocation(), Sound.SLIME_WALK, 0.5f, (float) (0.5 + 1.5 * ((getCharge(p) / 20) / 3d)));
+
+            if (getCharge(p) > 98) {
+                setcharge.put(p, getCharge(p));
+                charge.remove(p);
+                rc.put(p, false);
+                return;
+            }
+        }
+    }
+
+    @EventHandler
+    public void slimetoss(UpdateEvent e) {
+
+        if (e.getType() != UpdateType.TICK) {return;}
+
+        for (Player p : Bukkit.getOnlinePlayers()) {
+
+            if (!setcharge.containsKey(p)) {return;}
+            if (setcharge.get(p) <= 0) {return;}
+
+            if (getKit(p) != Kits.SLIME) {return;}
+
+            int exp = setcharge.get(p);
+
+            setcharge.remove(p);
+
+            if (exp > 80) tossSlime(p, 3);
+            else if (exp > 40) tossSlime(p, 2);
+            else if (exp > 1) tossSlime(p, 1);
+
+            return;
+
+        }
+
+    }
 
     public void thrown(Player p, Slime slime) {
         new BukkitRunnable() {
@@ -53,115 +128,20 @@ public class SlimeToss implements Listener {
     //TODO MAKE KNOCKBACK BASED OFF OF SIZE
     //TODO ADD METHOD FOR MAGMA CUBES
 
-    /*@EventHandler
-    public void SlimeToss(PlayerInteractEvent e) {
-        Player p = e.getPlayer();
-        if (getKit(p) == Kits.SLIME) {
-            ItemStack i = e.getPlayer().getItemInHand();
-            if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                if (i.getType() != null && i.getType() == Material.IRON_SWORD) {
-                    new BukkitRunnable() {
-                        public void run() {
-                            Boolean magmacube = Overheat.overheat.get(p);
-                            if (getKit(p) != Kits.SLIME) {
-                                cancel();
-                            }
-                            if (p.isBlocking()) {
-                                p.setExp((p.getExp() - .02f));
-                                l++;
-                                if (magmacube) {
-                                    p.getWorld().playSound(p.getLocation(), Sound.MAGMACUBE_WALK, 0.5f, (float) (0.5 + 1.5 * ((l / 20) / 3d)));
-                                } else {
-                                    p.getWorld().playSound(p.getLocation(), Sound.SLIME_WALK, 0.5f, (float) (0.5 + 1.5 * ((l / 20) / 3d)));
-                                }
-                                exp = exp - .01;
-                                if (exp < 0.01) {
-                                    if (magmacube) {
-                                        Slime slime = p.getWorld().spawn(p.getLocation(), MagmaCube.class);
-                                        slime.setSize(3);
-                                        slime.setVelocity(p.getLocation().getDirection().multiply(4));
-                                        cancel();
-                                    } else {
-                                        Slime slime = p.getWorld().spawn(p.getLocation(), Slime.class);
-                                        slime.setSize(3);
-                                        slime.setVelocity(p.getLocation().getDirection().multiply(4));
-                                        cancel();
-                                        thrown(p, slime);
-                                    }
-                                }
-                            }
+    private void tossSlime(Player p, Integer size) {
+        Slime slime = p.getWorld().spawn(p.getLocation().setDirection(p.getLocation().getDirection()), Slime.class);
+        slime.setSize(size);
+        slime.setVelocity(p.getLocation().getDirection().multiply(4));
+        thrown(p, slime);
+    }
 
+    private void addCharge(Player p, Integer add) {
+        charge.put(p, getCharge(p) + add);
+    }
 
-                            if (!p.isBlocking() || p.getExp() < 0.01) {
-                                if (magmacube) {
-                                    if (exp < 0.2) {
-                                        Slime slime = p.getWorld().spawn(p.getLocation(), MagmaCube.class);
-                                        slime.setSize(3);
-                                        slime.setVelocity(p.getLocation().getDirection().multiply(4));
-                                        cancel();
-                                    } else if (exp < 0.5) {
-                                        Slime slime = p.getWorld().spawn(p.getLocation(), MagmaCube.class);
-                                        slime.setSize(2);
-                                        slime.setVelocity(p.getLocation().getDirection().multiply(4));
-                                        cancel();
-                                    } else if (exp < 0.999) {
-                                        Slime slime = p.getWorld().spawn(p.getLocation(), MagmaCube.class);
-                                        slime.setSize(1);
-                                        slime.setVelocity(p.getLocation().getDirection().multiply(4));
-                                        cancel();
-                                    }
-                                } else {
-                                    if (exp < 0.2) {
-                                        Slime slime = p.getWorld().spawn(p.getLocation(), Slime.class);
-                                        slime.setSize(3);
-                                        slime.setVelocity(p.getLocation().getDirection().multiply(4));
-                                        cancel();
-                                        thrown(p, slime);
-                                    } else if (exp < 0.5) {
-                                        Slime slime = p.getWorld().spawn(p.getLocation(), Slime.class);
-                                        slime.setSize(2);
-                                        slime.setVelocity(p.getLocation().getDirection().multiply(4));
-                                        cancel();
-                                        thrown(p, slime);
-                                    } else if (exp < 0.999) {
-                                        Slime slime = p.getWorld().spawn(p.getLocation(), Slime.class);
-                                        slime.setSize(1);
-                                        slime.setVelocity(p.getLocation().getDirection().multiply(4));
-                                        cancel();
-                                        thrown(p, slime);
-                                    }
-                                }
-
-                                cancel();
-                            }
-
-
-                        }
-                    }.runTaskTimer((SmashMelee.getPlugin()), 0L, 1L);
-                    exp = 0.999;
-                    l = 0;
-                    //for () For loop for a Bukkit Runnable FOR each Slime : (
-                    //This will be aids!
-                }
-            }
-        }
-    }*/
-
-    @EventHandler
-    public void charge(UpdateEvent e) {
-
-        if (e.getType() != UpdateType.TICK) {return;}
-
-        for (Player p : Bukkit.getOnlinePlayers()) {
-
-            if (getKit(p) != Kits.SLIME) {return;}
-
-            if (!p.isBlocking()) {return;}
-
-
-
-        }
-
+    private Integer getCharge(Player p) {
+        if (charge.get(p) == null) return 0;
+        else return charge.get(p);
     }
 
 }
